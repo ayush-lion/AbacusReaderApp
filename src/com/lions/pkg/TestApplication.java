41ee8d40378e55af26caed97abf26e3d2f56b55b
dell <dell@192.168.1.11>
2017-08-10 02:36:08 -0700

    second

--- src/com/lions/pkg/TestApplication.java
+++ src/com/lions/pkg/TestApplication.java
@@ -10,17 +10,24 @@ public class TestApplication extends JApplet {
 
 private static final long serialVersionUID = 1L;
 
-    JPanel p1,p2,p3;
+    JPanel p1,p2,p3,p4;
 
     public void init(){         
-        setLayout(new GridLayout(3,1));
+        setLayout(new GridLayout(2,2));
         p1=new JPanel();
         p2=new JPanel();
-
+        p3=new JPanel();
+        p4=new JPanel();
+        
         p1.setBackground(Color.RED);
         p2.setBackground(Color.GREEN);
-        p3.
+        p3.setBackground(Color.CYAN);
+        p4.setBackground(Color.BLUE);
         add(p1);
         add(p2);
+        add(p3);
+        add(p3);
+        add(p4);
+        setSize(600,600);
     }   
 }
